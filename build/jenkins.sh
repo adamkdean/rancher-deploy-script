4b7e4efe7024f0e7f14b4fcaf18f2225e44aa08b
Adam K Dean <adamkdean@googlemail.com>
2016-03-01 17:53:20 +0000

    TEST

--- build/jenkins.sh
+++ build/jenkins.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+jsonq() { python -c "import sys,json; obj=json.load(sys.stdin); sys.stdout.write(json.dumps($1)"; }
+
 RANCHER_LOC="http://$RANCHER_ACCESS_KEY:$RANCHER_SECRET_KEY@192.168.1.48"
 SERVICE_NAME="alpine-nginx"
-URL="$RANCHER_LOC/v1/services?name=$SERVICE_NAME"
+SERVICE_URL="$RANCHER_LOC/v1/services?name=$SERVICE_NAME"
+SERVICE_JSON=$(curl $SERVICE_URL)
 
-curl $URL | python -c 'import json,sys;obj=json.load(sys.stdin);sys.stdout.write(json.dumps(obj["data"][0]))'
+LC=jsonq('obj["data"][0]["launchConfig"]')
 
-cat dump.txt
+echo $LC
+
+ #python -c 'import json,sys;obj=json.load(sys.stdin);sys.stdout.write(json.dumps(obj["data"][0]))'
