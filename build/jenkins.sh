dbe540e6d4f50e1caf9f3d1d19341c067f0d2d69
Adam K Dean <adamkdean@googlemail.com>
2016-03-01 17:42:47 +0000

    TEST

--- build/jenkins.sh
+++ build/jenkins.sh
@@ -4,6 +4,6 @@ RANCHER_LOC="http://$RANCHER_ACCESS_KEY:$RANCHER_SECRET_KEY@192.168.1.48"
 SERVICE_NAME="alpine-nginx"
 URL="$RANCHER_LOC/v1/services?name=$SERVICE_NAME"
 
-curl $URL | python -c 'import json,sys;obj=json.load(sys.stdin);print obj[0]["data"][0]' > dump
+curl $URL | python -c 'import json,sys;obj=json.load(sys.stdin);print obj' > dump
 
 cat dump
