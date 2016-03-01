33ec570c5984dc237443c9a2c7430aab96a87645
Adam K Dean <adamkdean@googlemail.com>
2016-03-01 17:36:44 +0000

    TEST

--- build/jenkins.sh
+++ build/jenkins.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-echo "NOT IMPLEMENTED YET"
-exit 2
+PATH=$PATH:$WORKSPACE/bin
+
+RANCHER_LOC="http://$RANCHER_ACCESS_KEY:$RANCHER_SECRET_KEY@192.168.1.48"
+SERVICE_NAME="alpine-nginx"
+URL="$RANCHER_LOC/v1/services?name=$SERVICE_NAME"
+
+curl $URL | jsawk 'return this.data[0]' > test.txt
+cat test.txt
