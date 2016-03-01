4966a2d0be3765d3c49158e0551b66ae2d6897b0
Adam K Dean <adamkdean@googlemail.com>
2016-03-01 18:45:51 +0000

    TEST

--- build/jenkins.sh
+++ build/jenkins.sh
@@ -13,12 +13,11 @@ SERVICE_JSON=$(curl $SERVICE_URL)
 
 LINKS_SELF==$(echo $SERVICE_JSON | jsonq 'obj["data"][0]["links"]["self"]' | sed -e 's/^"//'  -e 's/"$//')
 ACTIONS_UPGRADE=$(echo $SERVICE_JSON | jsonq 'obj["data"][0]["actions"]["upgrade"]' | sed -e 's/^"//'  -e 's/"$//')
-# ACTIONS_FINISH_UPGRADE=$(echo $SERVICE_JSON | jsonq 'obj["data"][0]["actions"]["finishupgrade"]' | sed -e 's/^"//'  -e 's/"$//')
 
 UPGRADE_BATCH_SIZE=1
 UPGRADE_INTERVAL_MILLIS=2000
 UPGRADE_START_FIRST="false"
-UPGRADE_URL="$RANCHER_LOC/v1${ACTIONS_UPGRADE#*/v1}"
+#UPGRADE_URL="$RANCHER_LOC/v1${ACTIONS_UPGRADE#*/v1}"
 UPGRADE_LC=$(echo $SERVICE_JSON | jsonq 'obj["data"][0]["launchConfig"]')
 UPGRADE_SLC=$(echo $SERVICE_JSON | jsonq 'obj["data"][0]["secondaryLaunchConfigs"]')
 
@@ -30,7 +29,7 @@ BODY="{ \"inServiceStrategy\": { \
   \"secondaryLaunchConfigs\": $UPGRADE_SLC } }"
 
 echo "[Upgrading $SERVICE_NAME]"
-curl -H "Content-Type: application/json" -X POST -d "$BODY" $UPGRADE_URL
+curl -H "Content-Type: application/json" -X POST -d "$BODY" $ACTIONS_UPGRADE
 
 echo "[Waiting for service $SERVICE_NAME to upgrade]"
 wait4upgrade() {
@@ -46,4 +45,6 @@ wait4upgrade() {
 }
 wait4upgrade
 
-echo "DONE!"
+ACTIONS_FINISH_UPGRADE=$(curl $LINKS_SELF | jsonq 'obj["actions"]["finishupgrade"]' | sed -e 's/^"//'  -e 's/"$//')
+
+echo "DONE, ACTIONS_FINISH_UPGRADE is $ACTIONS_FINISH_UPGRADE"
